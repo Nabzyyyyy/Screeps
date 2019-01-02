a16375ff8124bd29029bf2a7383d0d90f1e00f56
Nabil Sawan <nsawan777@gmail.com>
2019-01-02 11:48:17 -0500

    Added builder role to main.js

--- main.js
+++ main.js
@@ -11,5 +11,8 @@ module.exports.loop = function () {
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
+		if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
     }
 }
